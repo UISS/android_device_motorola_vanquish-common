7f0e6d03dc5b2032a45a040501833242cf0319fe
Emerson Pinter <dev@pinter.com.br>
2013-04-23 00:21:44 -0300

    vanquish-common will not be used anymore
    Change-Id: I934df075fa2c538362015a388eb2e1c929a1ec41

--- BoardConfig.mk
+++
@@ -1,5 +0,0 @@
--include device/motorola/msm8960-common/BoardConfigCommon.mk
-
-LOCAL_PATH := device/motorola/vanquish-common
-
-BOARD_HAS_NO_SELECT_BUTTON := true
--- device.mk
+++
@@ -1,7 +0,0 @@
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-$(call inherit-product, device/motorola/msm8960-common/msm8960.mk)
-
-LOCAL_PATH := device/motorola/vanquish-common
